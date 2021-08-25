a97f11133de475563cdd174eee410dbb973dfc5c
Arslkhan <arslkhan5@gmail.com>
2021-08-26 03:04:56 +0500

    CMS pages done

--- src/themes/default/pages/CmsPage.vue
+++ src/themes/default/pages/CmsPage.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="cms-page">
-    <header class="bg-cl-secondary py35 pl20">
+    <header class="bg-cl-secondary py25">
       <div class="container">
         <div class="row middle-sm">
-          <h1 class="col-sm-9 category-title mb10">
+          <h1 class="col-sm-9 category-title">
             {{ cmsPageContent.title }}
           </h1>
         </div>
@@ -25,3 +25,46 @@ export default {
   mixins: [CmsPage]
 }
 </script>
+
+<style lang="scss" >
+#cms-page {
+ .main-returns{
+   h4, .days-year{
+     font-weight: 700;
+     font-size:24px;
+   }
+   p {
+     font-weight:500;
+     font-size:20px;
+     font-family: 'Raleway';
+   }
+   .return-Links{
+     font-size:24px;
+     a{
+       color:#98694B;
+       text-decoration: underline;
+     }
+   }
+ }
+ .main-terms{
+   p, span{
+     font-weight:500 !important;
+     font-size:20px;
+     font-family: 'Raleway';
+   }
+   .terms-top{
+     font-weight: 700;
+   }
+ }
+ .main-policy{
+   .img-costa{
+     text-align: center;
+   }
+    p, span, ol, li{
+     font-weight:500 !important;
+     font-size:20px;
+     font-family: 'Raleway';
+   }
+ }
+}
+</style>
