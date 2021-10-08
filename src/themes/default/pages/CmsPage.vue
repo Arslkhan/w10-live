e544041fbe3a7bff1ce1e1444985cfa7a21367a5
arslanki5 <arsalan.khan@ki5.co.uk>
2021-10-08 23:52:10 +0500

    cms header removed

--- src/themes/default/pages/CmsPage.vue
+++ src/themes/default/pages/CmsPage.vue
@@ -1,14 +1,14 @@
 <template>
   <div id="cms-page">
-    <header class="bg-cl-secondary py25">
-      <div class="container">
-        <div class="row middle-sm">
-          <h1 class="col-sm-9 category-title">
-            {{ cmsPageContent.title }}
-          </h1>
-        </div>
-      </div>
-    </header>
+<!--    <header class="bg-cl-secondary py25">-->
+<!--      <div class="container">-->
+<!--        <div class="row middle-sm">-->
+<!--          <h1 class="col-sm-9 category-title">-->
+<!--            {{ cmsPageContent.title }}-->
+<!--          </h1>-->
+<!--        </div>-->
+<!--      </div>-->
+<!--    </header>-->
     <div class="container pb60" v-html="cmsPageContent.content" />
   </div>
 </template>
