2c0db5c7132ba173ef136f4952999087dfca5c41
arslkhan <arslkhan5@gmail.com>
2021-10-19 10:30:45 +0500

    overlay changes

---
+++ src/themes/default/components/core/HomeOverlay.vue
@@ -0,0 +1,94 @@
+<template>
+        <div class="row">
+            <div class="center-Content overlay-Left">
+                <a class="website-Link" href="https://w10.world/">Visit W10 Drinkware Collection</a>
+                <img class="w10Logo" src="/assets/w10Logo.svg">
+            </div>
+            <div class="center-Content overlay-Right">
+                <a class="website-Link" href="#">Visit Costa Coffee Drinkware</a>
+                <img class="costaCoffee" src="/assets/costaCoffee.svg">
+                <img class="costaCoffee-cup" src="/assets/costaCoffee-cup.svg">
+            </div>
+        </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+
+    mounted () {
+        this.HomeScrollDisable()
+    },
+
+   methods: {
+       HomeScrollDisable () {
+           if(document.querySelector('.overlayHome'));
+           {
+               document.querySelector('body').style.overflow = "hidden";
+           }
+       }
+  },
+}
+</script>
+<style lang="scss" scoped>
+    .center-Content{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+
+    }
+    .overlay-Right{
+        background: #6D1F37;
+        width: 50%;
+    }
+    .overlay-Left{
+        background: #000000;
+        width: 50%;
+    }
+    .w10Logo{
+        width: 280px;
+          @media (max-width:767px){
+            width: 150px;
+          }
+    }
+    .costaCoffee{
+        width:265px;
+        padding-bottom: 45px;
+         @media (max-width:767px){
+            padding-bottom: 20px;
+            width: 140px;
+         }
+    }
+    .costaCoffee-cup{
+        width: 65px;
+          @media (max-width:767px){
+              width: 40px;
+          }
+    }
+    .website-Link{
+        color:white;
+        text-decoration: underline;
+        font-size: 24px;
+        text-align: center;
+        font-family: 'Raleway-Regular';
+        padding-bottom: 25px;
+        @media (max-width:767px){
+            font-size: 18px;
+        }
+    }
+    .overlay-Right{
+        .website-Link{
+            padding-bottom: 45px;
+            @media (max-width:767px){
+                padding-bottom: 20px;
+            }
+        }
+    }
+    a.underline:after, a:not(.no-underline):hover:after{
+        content: none;
+    }
+</style>
--- src/themes/default/layouts/Default.vue
+++ src/themes/default/layouts/Default.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="default-layout">
     <overlay v-if="overlayActive" />
+    <div v-if="currPath && currPath === '/'" class="overlayHome">
+      <homeOverlay />
+    </div>
     <loader />
     <div id="viewport" class="w-100 relative">
       <main-header />
@@ -31,97 +34,127 @@
       <sign-up />
       <cookie-notification />
       <offline-badge />
-      <order-confirmation :orders-data="ordersData" v-if="loadOrderConfirmation" />
+      <order-confirmation
+        :orders-data="ordersData"
+        v-if="loadOrderConfirmation"
+      />
     </div>
     <vue-progress-bar />
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import AsyncSidebar from 'theme/components/theme/blocks/AsyncSidebar/AsyncSidebar.vue'
-import MainHeader from 'theme/components/core/blocks/Header/Header.vue'
-import MainFooter from 'theme/components/core/blocks/Footer/Footer.vue'
-import Overlay from 'theme/components/core/Overlay.vue'
-import Loader from 'theme/components/core/Loader.vue'
-import Notification from 'theme/components/core/Notification.vue'
-import SignUp from 'theme/components/core/blocks/Auth/SignUp.vue'
-import CookieNotification from 'theme/components/core/CookieNotification.vue'
-import OfflineBadge from 'theme/components/core/OfflineBadge.vue'
-import { isServer } from '@vue-storefront/core/helpers'
-import Head from 'theme/head'
-import config from 'config'
-import Login from '@vue-storefront/core/compatibility/components/blocks/Auth/Login'
+import HomeOverlay from "theme/components/core/HomeOverlay.vue";
+import { mapState } from "vuex";
+import AsyncSidebar from "theme/components/theme/blocks/AsyncSidebar/AsyncSidebar.vue";
+import MainHeader from "theme/components/core/blocks/Header/Header.vue";
+import MainFooter from "theme/components/core/blocks/Footer/Footer.vue";
+import Overlay from "theme/components/core/Overlay.vue";
+import Loader from "theme/components/core/Loader.vue";
+import Notification from "theme/components/core/Notification.vue";
+import SignUp from "theme/components/core/blocks/Auth/SignUp.vue";
+import CookieNotification from "theme/components/core/CookieNotification.vue";
+import OfflineBadge from "theme/components/core/OfflineBadge.vue";
+import { isServer } from "@vue-storefront/core/helpers";
+import Head from "theme/head";
+import config from "config";
+import Login from "@vue-storefront/core/compatibility/components/blocks/Auth/Login";
 
-const SidebarMenu = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-sidebar-menu" */ 'theme/components/core/blocks/SidebarMenu/SidebarMenu.vue')
-const Microcart = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-microcart" */ 'theme/components/core/blocks/Microcart/Microcart.vue')
-const Wishlist = () => import(/* webpackPreload: true */ /* webpackChunkName: "vsf-wishlist" */ 'theme/components/core/blocks/Wishlist/Wishlist.vue')
-const SearchPanel = () => import(/* webpackChunkName: "vsf-search-panel" */ 'theme/components/core/blocks/SearchPanel/SearchPanel.vue')
-const OrderConfirmation = () => import(/* webpackChunkName: "vsf-order-confirmation" */ 'theme/components/core/blocks/Checkout/OrderConfirmation.vue')
+const SidebarMenu = () =>
+  import(
+    /* webpackPreload: true */ /* webpackChunkName: "vsf-sidebar-menu" */ "theme/components/core/blocks/SidebarMenu/SidebarMenu.vue"
+  );
+const Microcart = () =>
+  import(
+    /* webpackPreload: true */ /* webpackChunkName: "vsf-microcart" */ "theme/components/core/blocks/Microcart/Microcart.vue"
+  );
+const Wishlist = () =>
+  import(
+    /* webpackPreload: true */ /* webpackChunkName: "vsf-wishlist" */ "theme/components/core/blocks/Wishlist/Wishlist.vue"
+  );
+const SearchPanel = () =>
+  import(
+    /* webpackChunkName: "vsf-search-panel" */ "theme/components/core/blocks/SearchPanel/SearchPanel.vue"
+  );
+const OrderConfirmation = () =>
+  import(
+    /* webpackChunkName: "vsf-order-confirmation" */ "theme/components/core/blocks/Checkout/OrderConfirmation.vue"
+  );
 
 export default {
   mixins: [Login],
-  data () {
+  data() {
     return {
       loadOrderConfirmation: false,
       ordersData: [],
       Microcart,
       Wishlist,
       SearchPanel,
-      SidebarMenu
-    }
+      SidebarMenu,
+    };
   },
   computed: {
     ...mapState({
-      overlayActive: state => state.ui.overlay,
-      isSearchPanelOpen: state => state.ui.searchpanel,
-      isSidebarOpen: state => state.ui.sidebar,
-      isMicrocartOpen: state => state.ui.microcart,
-      isWishlistOpen: state => state.ui.wishlist
-    })
+      overlayActive: (state) => state.ui.overlay,
+      isSearchPanelOpen: (state) => state.ui.searchpanel,
+      isSidebarOpen: (state) => state.ui.sidebar,
+      isMicrocartOpen: (state) => state.ui.microcart,
+      isWishlistOpen: (state) => state.ui.wishlist,
+    }),
+    currPath() {
+      return this.$router.currentRoute.path;
+    },
   },
   methods: {
-    onOrderConfirmation (payload) {
-      this.loadOrderConfirmation = true
-      this.ordersData = payload
-      this.$bus.$emit('modal-show', 'modal-order-confirmation')
+    onOrderConfirmation(payload) {
+      this.loadOrderConfirmation = true;
+      this.ordersData = payload;
+      this.$bus.$emit("modal-show", "modal-order-confirmation");
     },
-    fetchMenuData () {
-      return this.$store.dispatch('category-next/fetchMenuCategories', {
-        level: config.entities.category.categoriesDynamicPrefetch && config.entities.category.categoriesDynamicPrefetchLevel >= 0
-          ? config.entities.category.categoriesDynamicPrefetchLevel
-          : null,
-        skipCache: isServer
-      })
+    fetchMenuData() {
+      return this.$store.dispatch("category-next/fetchMenuCategories", {
+        level:
+          config.entities.category.categoriesDynamicPrefetch &&
+          config.entities.category.categoriesDynamicPrefetchLevel >= 0
+            ? config.entities.category.categoriesDynamicPrefetchLevel
+            : null,
+        skipCache: isServer,
+      });
+    },
+  },
+  serverPrefetch() {
+    return this.fetchMenuData();
+  },
+  mounted() {
+    console.log("forForgetPasswrod", this.$route.query.id === "forget");
+    if (this.$route.query.id === "forget") {
+      console.log(
+        "forForgetPasswrod inside",
+        this.$route.query && this.$route.query.id === "forget"
+      );
+      this.$bus.$emit("modal-show", "modal-signup");
+      this.callForgotPassword();
+      this.$store.commit("ui/setAuthElem", "forgot-pass");
+      console.log(
+        "forForgetPasswrod last",
+        this.$route.query && this.$route.query.id === "forget"
+      );
     }
   },
-  serverPrefetch () {
-    return this.fetchMenuData()
-  },
-  mounted () {
-    console.log('forForgetPasswrod', this.$route.query.id === 'forget')
-    if (this.$route.query.id === 'forget') {
-      console.log('forForgetPasswrod inside', this.$route.query && this.$route.query.id === 'forget')
-      this.$bus.$emit('modal-show', 'modal-signup')
-      this.callForgotPassword()
-      this.$store.commit('ui/setAuthElem', 'forgot-pass')
-      console.log('forForgetPasswrod last', this.$route.query && this.$route.query.id === 'forget')
-    }
-  },
-  beforeMount () {
+  beforeMount() {
     // Progress bar on top of the page
     this.$router.beforeEach((to, from, next) => {
-      this.$Progress.start()
-      this.$Progress.increase(40)
-      next()
-    })
+      this.$Progress.start();
+      this.$Progress.increase(40);
+      next();
+    });
     this.$router.afterEach((to, from) => {
-      this.$Progress.finish()
-    })
-    this.$bus.$on('offline-order-confirmation', this.onOrderConfirmation)
+      this.$Progress.finish();
+    });
+    this.$bus.$on("offline-order-confirmation", this.onOrderConfirmation);
   },
-  beforeDestroy () {
-    this.$bus.$off('offline-order-confirmation', this.onOrderConfirmation)
+  beforeDestroy() {
+    this.$bus.$off("offline-order-confirmation", this.onOrderConfirmation);
   },
   metaInfo: Head,
   components: {
@@ -135,9 +168,23 @@ export default {
     CookieNotification,
     OfflineBadge,
     OrderConfirmation,
-    AsyncSidebar
-  }
-}
+    AsyncSidebar,
+  },
+};
 </script>
 
 <style lang="scss" src="theme/css/main.scss"></style>
+<style scoped>
+.overlayHome {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 100%;
+  width: 100%;
+  opacity: 0.96;
+  background-color: #000000;
+  z-index: 4;
+}
+</style>
